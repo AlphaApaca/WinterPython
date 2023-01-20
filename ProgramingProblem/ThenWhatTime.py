2772fdd7263cbc39e208d7112c1724082674e09d
Alphalpaca <2684700064@qq.com>
2023-01-20 22:20:31 +0800

    GoodNight

--- ProgramingProblem/ThenWhatTime.py
+++ ProgramingProblem/ThenWhatTime.py
@@ -12,8 +12,15 @@ str = input().split(" ")
 time = int(str[0])
 nmin = int(str[1])
 
-if (time % 100 + next % 60) >= 0:
-            a = ((time / 100 + (next / 60)) + (time % 100 + next % 60) / 60) * 100 + (time % 100 + next % 60) % 60
+# p = int(time % 100)
+# y = int(next % 60)
+
+if (time % 100 + nmin % 60) >= 0:
+            a = ((time / 100 + (nmin / 60)) + (time % 100 + nmin % 60) / 60) * 100 + (time % 100 + nmin % 60) % 60
             judge(a)
 else:
-    a = ((time / 100 + (next / 60)) + ((time % 100 + next % 60) / 60 - 1)) * 100 + 60 + (time % 100 + next % 60)
+    a = ((time / 100 + (nmin / 60)) + ((time % 100 + nmin % 60) / 60 - 1)) * 100 + 60 + (time % 100 + nmin % 60)
+    judge(a)
+# 离谱的事情出现了，怎么会有相同的运算式不一样的结果的
+# 明天早上起来再搞
+# 直接一个晚的安
