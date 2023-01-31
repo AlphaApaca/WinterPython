feada68b33f4a8549493915f6c4a6913b9fcbb1a
Alphalpaca <2684700064@qq.com>
2023-01-31 21:24:53 +0800

    TouchFish

--- ProgramingProblem/Web/test0.py
+++ ProgramingProblem/Web/test0.py
@@ -1,6 +1,10 @@
+import time
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+# from selenium.webdriver.chrome.service import Service
 
-wd = webdriver.Chrome(service=Service(r'd:/tools/chromedriver.exe'))
-
+wd = webdriver.Chrome()
+# wd = webdriver.Chrome(service=Service(r'd:\tools\chromedriver.exe'))
 wd.get('https://www.baidu.com')
+
+input()
+# time.sleep(100000)
